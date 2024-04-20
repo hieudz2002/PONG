583b024d0b6536bb46cd439ac3cdbf1ffb507e7e
hieudz2002 <109191912+hieudz2002@users.noreply.github.com>
2024-04-20 10:59:16 +0700

    Add files via upload

---
+++ Menu_Controller.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu_Controller : MonoBehaviour
+{
+    public Button Play_Button;
+    public Button Quit_Button;
+    
+    public void Player_Game()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Quit_Game()
+    {
+
+        Application.Quit();
+    }
+}
